63d833098f567fdd2ed7f46163a37ab03f0ed973
Carlos Barrero Martínez <barrerot@gmail.com>
2024-08-09 13:39:29 +0200

    admincreation

---
+++ crear_admin.php
@@ -0,0 +1,23 @@
+<?php
+include 'config.php'; // Asegúrate de que config.php tiene los datos correctos para conectar a la base de datos
+
+// Datos del administrador
+$nombre = 'Carlos';
+$apellido = 'Barrero';
+$email = 'barrerot@gmail.com';
+$telefono = '699490161';
+$contrasena = password_hash('adminpassword', PASSWORD_BCRYPT); // Encripta la contraseña
+$tipo = 'administrador';
+
+// Consulta SQL para insertar el usuario administrador
+$sql = "INSERT INTO jugadores (nombre, apellido, email, telefono, contrasena, tipo) 
+        VALUES ('$nombre', '$apellido', '$email', '$telefono', '$contrasena', '$tipo')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Usuario administrador creado con éxito.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
